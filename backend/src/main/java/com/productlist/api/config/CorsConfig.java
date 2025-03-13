f088460fafd22477e303d14e0dd0c407b2b0e61d
Tobias Rödebaek <tobias.rodeback@gmail.com>
2025-03-13 22:29:27 +0100

    Added CORS-origin for 127.0.0.1:5173

--- backend/src/main/java/com/productlist/api/config/CorsConfig.java
+++ backend/src/main/java/com/productlist/api/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
